197ad3da96643e1b248400b67acb06c4c1a4a122
qmei1123 <qmei@bu.edu>
2017-12-01 12:39:27 -0500

    initial commit

---
+++ Assets/js.js
@@ -0,0 +1,12 @@
+function makeNewPosition(){
+    
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 50;
+    var w = $(window).width() - 50;
+    
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
+}
